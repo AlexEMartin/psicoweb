8268aa8f2bab2ce634f5b91ef63d5e78b403a0f2
alexmartin <alexeusebiomartin@gmail.com>
2022-11-27 18:58:07 -0300

    Animation

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css'
 import { SideBar } from "./sidebar/SideBar"
 import './Home.scss';
 import terapeuta from '../images/terapeuta.png'
@@ -7,28 +10,30 @@ import Footer from "./footer/Footer";
 
 const Home = () => {
 
+    useEffect(() => {AOS.init({duration: 2000})}, [])
+
     return (
         <div className='home'>
             <SideBar />
-            <h1 className='home-title'>Welcome</h1>
-            <p className='home-p1'> 
+            <h1 className='home-title' data-aos='fade-up'>Welcome</h1>
+            <p className='home-p1' data-aos='fade-up'> 
                 Mi nombre es Alex y es un placer tenerte por mi sitio 😊
                 Soy Psicólogo y Software Developer de Argentina 🇦🇷
                 He hecho yo mismo este sitio web para recibirte 🙌🏼
             </p>
-            <div className='professions'>
+            <div className='professions' data-aos='fade-up'>
                 <img className='sticker' src={terapeuta} alt="psicologo" />
             </div>
-            <h1 className='home-title2'>Sobre mí</h1>
-            <p className='home-p1'>
+            <h1 className='home-title2' data-aos='fade-up'>Sobre mí</h1>
+            <p className='home-p1' data-aos='fade-up'>
                 Licenciado en Psicología (Universidad Nacional de Córdoba, Argentina), especialista en Psicoterapia Cognitivo Conductual 🧠
                 Breve y Focalizada. Trabajo actualmente realizando Psicoterapia Online 💻 
                 Tuve la suerte de experimentar vivir en culturas diferentes, lo que me brindó
                 el aprendizaje y la posibilidad de trabajar con personas de diferentes partes del mundo 🌍
 
             </p>
-            <span className='home-p1'>Entre ellas, personas de : </span>
-            <ul className='flag-container'>
+            <span className='home-p1' data-aos='fade-up'>Entre ellas, personas de : </span>
+            <ul className='flag-container' data-aos='fade-up'>
                 <li className='flag'>🇦🇷</li>
                 <li className='flag'>🇨🇴</li>
                 <li className='flag'>🇨🇱</li>
@@ -40,8 +45,8 @@ const Home = () => {
                 <li className='flag'>🇵🇾</li>
                 <li className='flag'>🇲🇽</li>
             </ul>
-            <button className='button'>Contacto</button>
-            <img className='pamplona' src={profile} alt="Alex" />
+            <button className='button' data-aos='fade-up'>Contacto</button>
+            <img className='pamplona' src={profile} alt="Alex" data-aos='fade-up' />
             <Footer/>
         </div>
     )
